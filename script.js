5e0ecda4fec77539e21587ee540c3e99c024291e
Martin Unger <martin-unger2@live.at>
2023-07-26 07:46:05 +0200

    render Function + first css styling

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let fields = [
     null,
-    null,
+    "circle",
     null,
     null,
     null,
@@ -10,4 +10,29 @@ let fields = [
     null,
 ];
 
-f
+function init(){
+    render();
+};
+
+function render() {
+    const contentDiv = document.getElementById("content");
+
+    let tableHTML = "<table>";
+    for (let i = 0; i < 3; i++) {
+        tableHTML += "<tr>";
+        for (let j = 0; j < 3; j++) {
+            const index = i * 3 +j;
+            let symbol = "";
+            if(fields[index] === "circle") {
+                symbol = "o";
+            }
+            else if (fields[index] === "cross") { 
+                symbol = "x";
+            }
+            tableHTML += `<td>${symbol}</td>`;
+        }
+        tableHTML += "</tr>";
+    }
+    tableHTML += "</table>";
+    contentDiv.innerHTML = tableHTML;
+}
